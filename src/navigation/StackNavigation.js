9e01a6044aa3cce26b94a51686fdd232db7ecd55
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2021-02-16 16:10:38 -0600

    Se agregó botón de hambuerguesa

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IconButton } from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Movie from '../screens/Movie';
@@ -8,33 +9,47 @@ import Search from '../screens/Search';
 
 const Stack = createStackNavigator();
 
-export default function StackNavigation() {
+export default function StackNavigation(props) {
+
+    const { navigation } = props;
+
+    console.log(props);
+
+    const buttonLeft = () => {
+        return (
+            <IconButton
+                icon='menu'
+                onPress={() => navigation.openDrawer()}
+            />
+        )
+    }
+
     return (
         <Stack.Navigator>
             <Stack.Screen
                 name='home'
                 component={Home}
-                options={{ title: 'TheMovieApp' }}
+                options={{ title: 'TheMovieApp', headerLeft: () => buttonLeft() }}
             />
             <Stack.Screen
                 name='movie'
                 component={Movie}
-                options={{ title: '' }}
+                options={{ title: '', headerLeft: () => buttonLeft() }}
             />
             <Stack.Screen
                 name='news'
                 component={News}
-                options={{ title: 'Nuevas Películas' }}
+                options={{ title: 'Nuevas Películas', headerLeft: () => buttonLeft() }}
             />
             <Stack.Screen
                 name='popular'
                 component={Popular}
-                options={{ title: 'Películas Populares' }}
+                options={{ title: 'Películas Populares', headerLeft: () => buttonLeft() }}
             />
             <Stack.Screen
                 name='search'
                 component={Search}
-                options={{ title: '' }}
+                options={{ title: '', headerLeft: () => buttonLeft() }}
             />
         </Stack.Navigator>
     )
