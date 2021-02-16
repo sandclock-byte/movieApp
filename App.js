35bef9c4ab021bce06f64740ad7735266b4807f5
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2021-02-16 13:25:08 -0600

    Se configuro en android Vector Icons

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <PaperProvider>
       <SafeAreaView>
         <Text>Hola Mundo</Text>
+        <Button
+          icon='image'
+          mode='contained'
+          onPress={() => console.log('Pressed')}
+        >
+          Press me
+        </Button>
       </SafeAreaView>
     </PaperProvider>
   )
