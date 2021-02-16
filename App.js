c7fdad855af703ce3ae70c8e068688aaf29d5e48
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2021-02-16 13:56:08 -0600

    Se instaló React Navigation

--- App.js
+++ App.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Button } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <PaperProvider>
-      <SafeAreaView>
-        <Text>Hola Mundo</Text>
-        <Button
-          icon='image'
-          mode='contained'
-          onPress={() => console.log('Pressed')}
-        >
-          Press me
+      <NavigationContainer>
+        <SafeAreaView>
+          <Text>Hola Mundo</Text>
+          <Button
+            icon='image'
+            mode='contained'
+            onPress={() => console.log('Pressed')}
+          >
+            Press me
         </Button>
-      </SafeAreaView>
+        </SafeAreaView>
+      </NavigationContainer>
     </PaperProvider>
   )
 }
