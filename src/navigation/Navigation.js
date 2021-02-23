d1ae3307f20a4aa4988480f95c7fff47b4d20e30
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2021-02-23 16:58:14 -0600

    se edito drawer content

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -7,7 +7,10 @@ const Drawer = createDrawerNavigator();
 
 export default function Navigation() {
     return (
-        <Drawer.Navigator initialRouteName='app' drawerContent={(props) => <DrawerContent {...props} />}>
+        <Drawer.Navigator
+            initialRouteName='app'
+            drawerContent={(props) => <DrawerContent {...props} />}
+        >
             <Drawer.Screen name='app' component={StackNavigation} />
         </Drawer.Navigator>
     )
