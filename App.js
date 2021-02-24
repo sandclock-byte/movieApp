8776b2c8dec1d92c05bd11bf7528789efd6f925b
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2021-02-24 13:26:20 -0600

    Se agrego DarkTheme

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Button } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import {
+  Provider as PaperProvider,
+  DarkTheme as DarkThemePaper,
+  DefaultTheme as DefaultThemePaper,
+} from 'react-native-paper';
+import {
+  NavigationContainer,
+  DarkTheme as DarkThemeNavigation,
+  DefaultTheme as DefaultThemeNavigation,
+} from '@react-navigation/native';
 import Navigation from './src/navigation/Navigation';
 
 export default function App() {
+
+  DefaultThemePaper.colors.primary= '#1ae1f2';
+  DarkThemePaper.colors.primary= '#1ae1f2';
+  DarkThemePaper.colors.accent= '#1ae1f2';
+
+  DarkThemeNavigation.colors.background= '#192734';
+  DarkThemeNavigation.colors.card= '#15212b';
+
   return (
-    <PaperProvider>
-      <NavigationContainer>
+    <PaperProvider theme={DarkThemePaper}>
+      <NavigationContainer theme={DarkThemeNavigation}>
+        <StatusBar barStyle={'light-content'}/>
         <Navigation />
       </NavigationContainer>
     </PaperProvider>
