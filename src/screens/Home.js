4f971370d868a939ed4fed90c858f02e2a768d21
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2021-02-24 17:03:59 -0600

    Se consume api para obtener peliculas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getNewsMoviesApi } from '../api/movie';
 
 export default function Home() {
+
+    const [newMovies, setNewMovies] = useState(null);
+
+    console.log(newMovies);
+
+    useEffect(() => {
+        getNewsMoviesApi().then( response => {
+            setNewMovies(response.results);
+        });
+    }, []);
+
     return (
         <View>
             <Text>Estamos en Home</Text>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
+export const API_HOST = 'https://api.themoviedb.org/3';
 export const API_KEY = '1a89b4a484b7ff54841495b876bfc38b';
+export const LANG = 'es-LA';
