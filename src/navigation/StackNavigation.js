5c86ff1c95ef2189aa8c080de410d74889797b01
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2021-02-24 16:22:54 -0600

    Se agregó boton buscar

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -13,13 +13,36 @@ export default function StackNavigation(props) {
 
     const { navigation } = props;
 
-    const buttonLeft = () => {
+    const buttonLeft = (screen) => {
+
+        switch (screen) {
+            case 'search':
+            case 'movie':
+                return (
+                    <IconButton
+                        icon='arrow-left'
+                        onPress={() => navigation.goBack()}
+                    />
+                )
+
+            default:
+                return (
+                    <IconButton
+                        icon='menu'
+                        onPress={() => navigation.openDrawer()}
+                    />
+                );
+        }
+
+    }
+
+    const buttonRight = () => {
         return (
             <IconButton
-                icon='menu'
-                onPress={() => navigation.openDrawer()}
+                icon='magnify'
+                onPress={() => navigation.navigate('search')}
             />
-        )
+        );
     }
 
     return (
@@ -27,27 +50,46 @@ export default function StackNavigation(props) {
             <Stack.Screen
                 name='home'
                 component={Home}
-                options={{ title: 'TheMovieApp', headerLeft: () => buttonLeft() }}
+                options={{
+                    title: 'TheMovieApp',
+                    headerLeft: () => buttonLeft('home'),
+                    headerRight: () => buttonRight(),
+                }}
             />
             <Stack.Screen
                 name='movie'
                 component={Movie}
-                options={{ title: '', headerLeft: () => buttonLeft() }}
+                options={{
+                    title: '',
+                    headerLeft: () => buttonLeft('movie'),
+                    headerRight: () => buttonRight(),
+                }}
             />
             <Stack.Screen
                 name='news'
                 component={News}
-                options={{ title: 'Nuevas Películas', headerLeft: () => buttonLeft() }}
+                options={{
+                    title: 'Nuevas Películas',
+                    headerLeft: () => buttonLeft('news'),
+                    headerRight: () => buttonRight(),
+                }}
             />
             <Stack.Screen
                 name='popular'
                 component={Popular}
-                options={{ title: 'Películas Populares', headerLeft: () => buttonLeft() }}
+                options={{
+                    title: 'Películas Populares',
+                    headerLeft: () => buttonLeft('popular'),
+                    headerRight: () => buttonRight(),
+                }}
             />
             <Stack.Screen
                 name='search'
                 component={Search}
-                options={{ title: '', headerLeft: () => buttonLeft() }}
+                options={{
+                    title: '',
+                    headerLeft: () => buttonLeft('search'),
+                }}
             />
         </Stack.Navigator>
     )
