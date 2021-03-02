a82f1dfc3e93a5309bc5d6d53e3cb5876bef45e0
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2021-03-01 21:41:05 -0600

    Se despliega generos de peliculas

--- src/api/movies.js
+++ src/api/movies.js
@@ -11,3 +11,21 @@ export function getNewsMoviesApi(page = 1) {
             return result;
         });
 }
+
+export function getGenreMovieApi(idGenres) {
+    const url = `${API_HOST}/genre/list?api_key=${API_KEY}&language=${LANG}`;
+
+    return fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            const arrayGenres = [];
+            idGenres.forEach(id => {
+                result.genres.forEach(item => {
+                    if (item.id === id) arrayGenres.push(item.name);;
+                });
+            });
+            return arrayGenres;
+        });
+}
--- src/components/CarouselVertical.js
+++ src/components/CarouselVertical.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Image, Dimensions, TouchableWithoutFeedback } from 'react-native';
 import { Text, Title } from 'react-native-paper';
 import Carousel from 'react-native-snap-carousel';
+import { map, size } from 'lodash';
 import { BASE_PATH_IMG } from '../utils/constants';
+import { getGenreMovieApi } from '../api/movies';
 
 const { width } = Dimensions.get('window');
 const ITEM_WIDTH = Math.round(width * 0.7);
@@ -22,17 +24,35 @@ export default function CarouselVertical(props) {
 
 const RenderItem = (props) => {
     const { data } = props;
-    const { title, poster_path } = data.item;
+    const { title, poster_path, genre_ids } = data.item;
+    const [genres, setGenres] = useState(null);
     const imageUrl = `${BASE_PATH_IMG}/w500${poster_path}`;
+
+    useEffect(() => {
+        getGenreMovieApi(genre_ids)
+            .then(response => {
+                setGenres(response);
+            });
+    }, [])
 
     return (
         <TouchableWithoutFeedback onPress={() => console.log('Hola')}>
             <View style={styles.card}>
                 <Image
                     style={styles.image}
-                    source={{uri: imageUrl}}
+                    source={{ uri: imageUrl }}
                 />
                 <Title style={styles.title}>{title}</Title>
+                <View style={styles.genres}>
+                    {genres && (
+                        map(genres, (genre, index) => (
+                            <Text key={index} style={styles.genre}>
+                                {genre}
+                                {index !== size(genres) - 1 && ', '}
+                            </Text>
+                        ))
+                    )}
+                </View>
             </View>
         </TouchableWithoutFeedback>
     );
@@ -58,5 +78,15 @@ const styles = StyleSheet.create({
     title: {
         marginHorizontal: 10,
         marginTop: 10,
+    },
+
+    genres: {
+        flexDirection: 'row',
+        marginHorizontal: 10,
+    },
+
+    genre: {
+        fontSize: 12,
+        color: '#8997a5',
     },
 });
