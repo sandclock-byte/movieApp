6fcc537db69284ba28147fcd1e14462fa895b21d
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2021-03-01 20:58:35 -0600

    Se agregaron imagenes y titulos de peliculas en Carousel

--- src/components/CarouselVertical.js
+++ src/components/CarouselVertical.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, Dimensions, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, View, Image, Dimensions, TouchableWithoutFeedback } from 'react-native';
+import { Text, Title } from 'react-native-paper';
 import Carousel from 'react-native-snap-carousel';
+import { BASE_PATH_IMG } from '../utils/constants';
 
 const { width } = Dimensions.get('window');
 const ITEM_WIDTH = Math.round(width * 0.7);
@@ -19,13 +21,42 @@ export default function CarouselVertical(props) {
 }
 
 const RenderItem = (props) => {
-    const { } = props;
+    const { data } = props;
+    const { title, poster_path } = data.item;
+    const imageUrl = `${BASE_PATH_IMG}/w500${poster_path}`;
 
     return (
-        <View>
-            <Text>Pelicula</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={() => console.log('Hola')}>
+            <View style={styles.card}>
+                <Image
+                    style={styles.image}
+                    source={{uri: imageUrl}}
+                />
+                <Title style={styles.title}>{title}</Title>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    card: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+    },
+
+    image: {
+        width: '100%',
+        height: 450,
+        borderRadius: 20,
+    },
+
+    title: {
+        marginHorizontal: 10,
+        marginTop: 10,
+    },
+});
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const API_HOST = 'https://api.themoviedb.org/3';
 export const API_KEY = '1a89b4a484b7ff54841495b876bfc38b';
-export const LANG = 'es-LA';
+export const LANG = 'es-MX';
+export const BASE_PATH_IMG = 'https://image.tmdb.org/t/p/';
